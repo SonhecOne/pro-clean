54aa35ca3c53468e8e5ad7966074b51b34f666fd
SonhecOne <107044823+SonhecOne@users.noreply.github.com>
2026-04-02 11:08:22 -0500

    Add files via upload

---
+++ Index.tsx
@@ -0,0 +1,21 @@
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+import About from "@/components/About";
+import Services from "@/components/Services";
+import WhyChooseUs from "@/components/WhyChooseUs";
+import CTASection from "@/components/CTASection";
+import Footer from "@/components/Footer";
+
+const Index = () => (
+  <div className="min-h-screen">
+    <Navbar />
+    <Hero />
+    <About />
+    <Services />
+    <WhyChooseUs />
+    <CTASection />
+    <Footer />
+  </div>
+);
+
+export default Index;
